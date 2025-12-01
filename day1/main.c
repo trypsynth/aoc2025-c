1a437453b05eee915b9aeb56400f618bd6a6d51f
Quin Gillespie <trypsynth@gmail.com>
2025-12-01 04:38:59 -0700

    Complete the day 1 challenge.

--- day1/main.c
+++ day1/main.c
@@ -8,26 +8,19 @@ int main() {
 		return EXIT_FAILURE;
 	}
 	int pos = 50;
-	int zero_count = 0;
-	char buf[256];
-	while (fgets(buf, sizeof(buf), fp)) {
-		char dir;
-		int dist;
-		if (sscanf(buf, "%c%d", &dir, &dist) != 2) {
-			continue;
-		}
-		if (dir == 'L') {
-			pos = (pos - dist) % 100;
-			if (pos < 0) {
-				pos += 100;
+	long long zero_count = 0;
+	char dir;
+	int dist;
+	while (fscanf(fp, " %c%d", &dir, &dist) == 2) {
+		int step = (dir == 'L') ? -1 : 1;
+		for (int i = 0; i < dist; i++) {
+			pos = (pos + step + 100) % 100;
+			if (pos == 0) {
+				zero_count++;
 			}
-		} else if (dir == 'R') {
-			pos = (pos + dist) % 100;
-		}
-		if (pos == 0) {
-			zero_count++;
 		}
 	}
-	printf("%d\n", zero_count);
+	fclose(fp);
+	printf("%lld\n", zero_count);
 	return EXIT_SUCCESS;
 }
