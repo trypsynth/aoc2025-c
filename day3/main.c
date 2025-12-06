e95e7132f60fbb5fce3d530ed14973c90b20b60b
Quin Gillespie <trypsynth@gmail.com>
2025-12-06 07:09:15 -0700

    Day 3 part 2.

--- day3/main.c
+++ day3/main.c
@@ -1,18 +1,28 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
-void find_largest_pair(const char* s, int* result) {
-	int best = -1;
-	int best_tens = -1;
-	for (const char* p = s; *p; p++) {
-		int d = *p - '0';
-		if (best_tens != -1) {
-			int val = best_tens * 10 + d;
-			if (val > best) best = val;
+void find_largest_k(const char* s, int k, char* out) {
+	int n = strlen(s);
+	int to_remove = n - k;
+	int top = 0;
+	for (int i = 0; i < n; i++) {
+		char c = s[i];
+		while (top > 0 && to_remove > 0 && out[top - 1] < c) {
+			top--;
+			to_remove--;
 		}
-		if (d > best_tens) best_tens = d;
+		out[top++] = c;
 	}
-	*result = best;
+	out[k] = '\0';
+}
+
+long long parse_12_digits(const char* s) {
+	long long val = 0;
+	for (int i = 0; i < 12; i++) {
+		val = val * 10 + (s[i] - '0');
+	}
+	return val;
 }
 
 int main() {
@@ -21,13 +31,21 @@ int main() {
 		perror("fopen");
 		return EXIT_FAILURE;
 	}
-	int total = 0;
-	char line[128];
+	char line[256];
+	long long total_part1 = 0;
+	long long total_part2 = 0;
 	while (fscanf(f, "%s", line) == 1) {
-		int best = 0;
-		find_largest_pair(line, &best);
-		total += best;
+		char best2[3];
+		find_largest_k(line, 2, best2);
+		int p1 = (best2[0] - '0') * 10 + (best2[1] - '0');
+		total_part1 += p1;
+		char best12[64];
+		find_largest_k(line, 12, best12);
+		long long p2 = parse_12_digits(best12);
+		total_part2 += p2;
 	}
-	printf("%d\n", total);
+	fclose(f);
+	printf("%lld\n", total_part1);
+	printf("%lld\n", total_part2);
 	return EXIT_SUCCESS;
 }
