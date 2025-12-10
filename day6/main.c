1c26be7e611b2af27879e1eda19182f327d7313c
Quin Gillespie <trypsynth@gmail.com>
2025-12-10 08:34:06 -0700

    Day 6 part 2.

--- day6/main.c
+++ day6/main.c
@@ -42,7 +42,38 @@ int main() {
 		total += acc;
 		col = end;
 	}
+	long total2 = 0;
+	for(int col2 = 0; col2 < len; ) {
+		int valid = 0;
+		for(int r = 0; r < row_count; r++) if(!isspace((unsigned char)rows[r][col2])) valid = 1;
+		if(!valid) {
+			col2++;
+			continue;
+		}
+		int end2 = col2;
+		for( ; end2 < len; end2++) {
+			int v2 = 0;
+			for(int r = 0; r < row_count; r++) if(!isspace((unsigned char)rows[r][end2])) v2 = 1;
+			if(!v2) break;
+		}
+		char op2 = '?';
+		for(int c = col2; c < end2; c++) if(rows[row_count-1][c] == '+' || rows[row_count-1][c] == '*') op2 = rows[row_count-1][c];
+		long acc2 = (op2 == '*') ? 1 : 0;
+		for(int c = col2; c < end2; c++) {
+			char buf2[64];
+			int bi2 = 0;
+			for(int r = 0; r < row_count - 1; r++) if(isdigit((unsigned char)rows[r][c])) buf2[bi2++] = rows[r][c];
+			if(bi2 == 0) continue;
+			buf2[bi2] = 0;
+			long v = strtol(buf2, NULL, 10);
+			if(op2 == '+') acc2 += v;
+			else acc2 *= v;
+		}
+		total2 += acc2;
+		col2 = end2;
+	}
 	printf("%ld\n", total);
+	printf("%ld\n", total2);
 	for(int i = 0; i < row_count; i++) free(rows[i]);
 	return 0;
 }
